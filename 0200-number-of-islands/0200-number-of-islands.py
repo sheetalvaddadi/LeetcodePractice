7734a5540b0b62ba1fdd2e6545488e13a96d9aec
Sheetal Vaddadi <108904456+sheetalvaddadi@users.noreply.github.com>
2022-11-08 07:48:15 -0600

    Time: 794 ms (47.67%), Space: 16.3 MB (91.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,32 @@
+import collections
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_of_islands = 0
-        
+        num_islands =0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] =="1":
-                    self.dfs(grid,i,j)
-                    num_of_islands+=1
-                    
-        return num_of_islands
+                    self.bfs(grid, i,j)
+                    num_islands+=1
+        return num_islands
     
-    def dfs(self, grid,row,col):
-        grid[row][col] = "0"
+    def bfs(self, grid, i,j):
+        q = collections.deque()
+        q.appendleft((i,j))
         
-        if row+1 <len(grid) and grid[row+1][col] =="1":
-            self.dfs(grid,row+1,col)
-        if row-1 >=0 and grid[row-1][col] =="1":
-            self.dfs(grid,row-1,col)
-        if col+1 <len(grid[0]) and grid[row][col+1] =="1":
-            self.dfs(grid,row,col+1)
-        if col-1 >=0 and grid[row][col-1] =="1":
-            self.dfs(grid,row,col-1)
-    
+        while(q):
+            row,col = q.pop()
+            
+            if row+1 < len(grid) and grid[row+1][col] =="1":
+                grid[row+1][col] = "0"
+                q.appendleft((row+1,col))
+            if row-1 >=0 and grid[row-1][col] =="1":
+                grid[row-1][col] = "0"
+                q.appendleft((row-1,col))
+            if col+1 < len(grid[0]) and grid[row][col+1] =="1":
+                grid[row][col+1] = "0"
+                q.appendleft((row,col+1))
+            if col-1 >=0 and grid[row][col-1] =="1":
+                grid[row][col-1] = "0"
+                q.appendleft((row,col-1))
+
         
